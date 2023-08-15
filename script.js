4899bf3f191b281f4fc0c064c02d861c52e85527
Bhavesh zalke <zalkebhavesh@gmail.com>
2023-08-15 17:47:02 +0530

    changes are made

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var timer = 60;
-var score=0;
+var score=-10;
 var hitrn =0;
-
+var scoreReturn
+var highScore =scoreReturn
+var over 
 //make bubble using the function
 function makeBubble(){
     var clutter = "";
-    for(var i =0 ; i<=125;i++){
+    for(var i =0 ; i<=188;i++){
     var rn = Math.floor(Math.random()*10);
     clutter += `<div id="bubble">${rn}</div>`   
     }
     document.querySelector("#bottom").innerHTML=clutter;
 }
+makeBubble();
 
 //time of the game
 function runTimer(){
@@ -37,6 +40,8 @@ function increaaseScore(){
     score += 10;
     document.querySelector("#scoreVal").textContent=score;
 }
+
+//click on bubble 
 document.querySelector("#bottom")
 .addEventListener("click",function(dets){
     var clickedNumber =(Number(dets.target.textContent));
@@ -45,70 +50,97 @@ document.querySelector("#bottom")
         makeBubble();
         hit();
     }else{
-        makeBubble()
+        gameOver()
+        timer =0;
+        if(score > -10 ){
+            over.textContent=`Game Over Score :${Number(score)}`;
+        }
+        else{
+            over.textContent=`Game Over Score :${Number(score=0)}`;
+        }
     }
 })
 
+//high Score function
+function highSco() {
+    if(score>highScore){
+        scoreReturn = score
+    }
+    else if(score<highScore && score==highScore){
+        scoreReturn = highScore 
+    }
+}
+highSco()
+console.log(highScore)
 
 //to show the game over text
 function gameOver(){
-    var over = document.querySelector("#bottom");
-    over.textContent="Game Over";
+    over = document.querySelector("#bottom");
+    over.textContent=`Game Over Score :${Number(score)}`;
     over.style.color="green";
-    over.style.fontSize="100px";
+    over.style.fontSize="50px";
     over.style.fontFamily ="Times new Romen";
     over.style.fontWeight="900";
+    timer=0;
+    document.querySelector("#Timer").textContent=timer  
 }
-makeBubble();
 
 //start button
-document.querySelector("#StarBtn")
-.addEventListener("click",function(){
-    timer=60;
-    runTimer();
-    increaaseScore();
-    hit();
-    makeBubble();
-})
+function start(){
+    document.querySelector("#StarBtn")
+    .addEventListener("click",function(){
+        score = -10;
+        timer=60;
+        runTimer();
+        increaaseScore();
+        hit();
+        makeBubble();
+    })
+}
+start()
 
 //End button
 document.querySelector("#EndBtn")
 .addEventListener("click",function(){
     gameOver();
-    timer=0;
-    document.querySelector("#Timer").textContent=timer
+    if(score > -10 ){
+         over.textContent=`Game Over Score :${Number(score)}`;
+    }
+    else{
+        over.textContent=`Game Over Score :${Number(score=0)}`;
+    }
 })
 
-// gsap section 
-gsap.from("#pannel-top .elem",{
-    y:-50,
-    x:-50,
-    duration :0.5,
-    delay :1.5,
-    scale:0,
-    opacity:0,
-    stagger :0.5,
-})
-gsap.from("#bubble",{
-    y:-50,
-    opacity:0,
-    scale:3,
-    duration:1,
-    delay:0.5,
-})
-gsap.from("#pannel",{
-    scale:3,
-    opacity:0,
-    duration:0.5,
-    delay:1,
-    rotate : 360,
-})
-gsap.from("button",{
-    y:50,
-    x:30,
-    scale:2,
-    opacity:0,
-    duration:1,
-    delay : 2,
-    rotate : 180,
-})
+// // gsap section 
+// gsap.from("#pannel-top .elem",{
+//     y:-50,
+//     x:-50,
+//     duration :0.5,
+//     delay :1.5,
+//     scale:0,
+//     opacity:0,
+//     stagger :0.5,
+// })
+// gsap.from("#bubble",{
+//     y:-50,
+//     opacity:0,
+//     scale:3,
+//     duration:1,
+//     delay:0.5,
+// })
+// gsap.from("#pannel",{
+//     scale:3,
+//     opacity:0,
+//     duration:0.5,
+//     delay:1,
+//     rotate : 360,
+// })
+// gsap.from("button",{
+//     y:50,
+//     x:30,
+//     scale:2,
+//     opacity:0,
+//     duration:1,
+//     delay : 2,
+//     rotate : 180,
+// })
